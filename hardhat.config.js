697906921461276f3c69d11c28e994cb51098d42
abdxzi <azeezonline2013@gmail.com>
2024-05-30 00:17:05 +0530

    hardhat_node network

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,6 +13,10 @@ module.exports = {
     goerli: {
       url: process.env.TESTNET_RPC_URL_GOERLI,
       accounts: [process.env.TESTNET_PRIVATE_KEY],
+    },
+    hardhat_node: {
+      url: "http://127.0.0.1:8545/",
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"]
     }
   }
 };
